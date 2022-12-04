a40f5e2544ec36f65c17b1f25964f4686436d282
Gypsophila <2549153066@qq.com>
2022-12-04 13:45:05 +0800

    控制输出当前creep的采集能量

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,15 +2,18 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.store[RESOURCE_ENERGY] > 0) {
-	        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
-        }
-        else {
+        console.log(crerp.memory.role+":"+creep.store[RESOURCE_ENERGY]);
+	    if(creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
+            //采矿
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            //送资源给控制器
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
             }
         }
 	}
