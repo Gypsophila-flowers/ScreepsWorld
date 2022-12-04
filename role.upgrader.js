ec0056c518c37e4644808ea1651a86bd3533480a
Gypsophila <2549153066@qq.com>
2022-12-04 14:00:15 +0800

    修改控制creep的行动方式

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,18 +2,17 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        console.log(creep.memory.role+":"+creep.store[RESOURCE_ENERGY]);
-	    if(creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
+	    if(creep.store[RESOURCE_ENERGY] > 0) {
+            //送资源给控制器
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+        else {
             //采矿
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
-            }
-        }
-        else {
-            //送资源给控制器
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
             }
         }
 	}
