fc5e3e170e95c88c5369ca5190e685fbd15b90b7
Gypsophila <2549153066@qq.com>
2023-06-22 18:21:17 +0800

    修改数量

--- main.js
+++ main.js
@@ -96,11 +96,11 @@ const REPAIRER_BODY = MEDIUM_BODY
 const roleList = [
 	new Role(ROLE_HARVESTER, LITTLE_BODY, roleHarvester, 1),
 	new Role(ROLE_UPGRADER, UPGRADER_BODY, roleUpgrader, 2),
-	new Role(ROLE_SOLDIER, SOLDIER_BODY, roleSoldier, 2, true),
-	new Role(ROLE_MINER, MINER_BODY, roleMiner, 1),
-	new Role(ROLE_TRANSFER, TRANSFER_BODY, roleTranfer, 1),
-	new Role(ROLE_BUILDER, BUILDER_BODY, roleBuilder, 2),
-	new Role(ROLE_REPAIRER, REPAIRER_BODY, roleRepairer, 1),
+	new Role(ROLE_SOLDIER, SOLDIER_BODY, roleSoldier, 1, true),
+	new Role(ROLE_MINER, MINER_BODY, roleMiner, 2),
+	new Role(ROLE_TRANSFER, TRANSFER_BODY, roleTranfer, 0),
+	new Role(ROLE_BUILDER, BUILDER_BODY, roleBuilder, 1),
+	new Role(ROLE_REPAIRER, REPAIRER_BODY, roleRepairer, 0),
 ]
 
 // 是否可以spawn该role；用于控制role创建顺序
